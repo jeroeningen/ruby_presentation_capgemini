1cdb83e2d7032cbd1f5509047329429fe2ad1941
Jeroen van Ingen <jeroenvaningen@Jeroens-iMac.local>
2018-01-15 12:32:44 +0100

    Update for integration test

--- spec/features/order_spec.rb
+++ spec/features/order_spec.rb
@@ -4,42 +4,29 @@ feature "Create, read update and delete an order" do
     click_link "Nieuwe bestelling"
 
     fill_in "order_name", with: "test_order"
+    fill_in "product_name_0", with: "Bed"
+    fill_in "price_0", with: "300"
+    select 2, from: "quantity_0"
+    fill_in "product_name_1", with: "Deken"
+    fill_in "price_1", with: "100"
+    select 2, from: "quantity_1"
     click_button "Maak bestelling"
 
     expect(page).to have_content("Bestelling aangemaakt!")
+    expect(Order.count).to eq(1)
 
     click_link "Toon bestelling"
     expect(page).to have_content("Bestelling ##{Order.last.id}")
-    click_link "Nieuwe bestelregel"
-    fill_in "product_name", with: "Bed"
-    fill_in "price", with: "100"
-    select 2, from: "quantity"
-    click_button "Maak bestelregel"
+    expect(page).to have_content("Totaal bestelling: EUR. 500,00")
 
-    expect(page).to have_content("Bestelregel aangemaakt!")
-    expect(page).to have_content("Totaal bestelling: EUR. 200,00")
-    click_link "Verwijder bestelregel"
-    expect(page).to have_content("Totaal bestelling: EUR. 0,00")
-
-    click_link "Nieuwe bestelregel"
-    fill_in "product_name", with: "Bed"
-    fill_in "price", with: "100"
-    select 2, from: "quantity"
-    click_button "Maak bestelregel"
-
-    click_link "Toon besteregel"
-    expect(page).to have_content("2 stuks")
-
-    click_link "Toon bestelling"
-    expect(page).to have_content("Totaal bestelling: EUR. 200,00")
     click_link "Wijzig bestelling"
-    fill_in "product_name", with: "Bed2"
+    fill_in "product_name_0", with: "Bed2"
     click_button "Wijzig bestelling"
 
     expect(page).to have_content("Bestelling gewijzigd!")
     expect(page).to have_content("Bed2")
 
-    click_link "Delete order"
+    click_link "Verwijder bestelling"
     expect(page).to have_content("Bestelling verwijderd!")
     expect(Order.count).to eq(0)
   end
